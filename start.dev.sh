a29dd0cd5d2b168f8941af6431979cd6602be9b1
Wolmer Martins <wolmer.martins@octadesk.com>
2024-04-07 11:37:35 -0300

    chore: adds script to start development

---
+++ start.dev.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+start_dev() {
+  docker-compose watch
+}
+
+start_logging() {
+  has_started=0
+
+  while [ $has_started -eq 0 ];
+  do
+    docker-compose ps | grep "Up" && has_started=1;
+  done
+
+  while docker-compose ps | grep "Up";
+  do
+    docker-compose logs -f;
+  done
+}
+
+stop_dev() {
+  docker-compose down -v
+
+  trap - SIGINT
+}
+
+(trap 'stop_dev && kill 0' SIGINT; start_dev & start_logging)
