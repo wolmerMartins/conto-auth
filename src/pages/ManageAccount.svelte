f1d29ce7d823dd798d704ab72ca73d1469f1023f
Wolmer Martins <wolmermartins@hotmail.com>
2024-07-15 15:44:33 -0300

    feat: creates manage account page

---
+++ src/pages/ManageAccount.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import SignInAccount from '../components/SignInAccount.svelte'
+  import SignUpAccount from '../components/SignUpAccount.svelte'
+
+  export let isAccountAlreadyCreated: boolean
+  export let email: string
+</script>
+
+{#if isAccountAlreadyCreated}
+  <SignInAccount {email} />
+{:else}
+  <SignUpAccount {email} />
+{/if}
