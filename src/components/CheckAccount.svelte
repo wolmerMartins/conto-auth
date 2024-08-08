a26055e66adce331d318bd6d45c11d520033fcce
Wolmer Martins <wolmermartins@hotmail.com>
2024-08-08 17:45:06 -0300

    feat: implements check account action

--- src/components/CheckAccount.svelte
+++ src/components/CheckAccount.svelte
@@ -1,31 +1,36 @@
 <script lang="ts">
-  import { useAccounts } from '../hooks/use-accounts'
+  import { useAccounts, type AccountState } from '../hooks/use-accounts'
   import { isValidEmail } from '../services/accounts.service'
   import Button from './base/Button.svelte'
   import Input from './base/Input.svelte'
 
   let email: string
+  let isCheckingAccount = false
 
-  const { setEmail } = useAccounts()
+  function getState(state: AccountState): void {
+    isCheckingAccount = Boolean(state.isCheckingAccount)
+  }
+
+  const { checkAccount } = useAccounts(getState)
 
   function hasValidEmail(email: string): boolean {
     return Boolean(email && isValidEmail(email))
   }
 
-  function checkAccount(): void {
+  function handleCheckAccount(): void {
     if (!hasValidEmail(email)) return
 
-    setEmail(email)
+    checkAccount(email)
   }
 
-  $: shouldDisableButton = !hasValidEmail(email)
+  $: shouldDisableButton = !hasValidEmail(email) || isCheckingAccount
 </script>
 
 <Input label="email" bind:value={email} />
 
 <Button
   disabled={shouldDisableButton}
-  on:click={checkAccount}
+  on:click={handleCheckAccount}
 >
   next
 </Button>
