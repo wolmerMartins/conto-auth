997048b20c99fe5b51c57bdb681d22d2161d37ff
Wolmer Martins <wolmermartins@hotmail.com>
2024-10-21 18:20:08 -0300

    feat: adds the check account function to the use accounts hook

--- src/hooks/tests/use-accounts.test.ts
+++ src/hooks/tests/use-accounts.test.ts
@@ -1,29 +1,13 @@
 import type { Unsubscriber } from 'svelte/store'
-import { describe, expect, test, vi, type TestContext } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
 
-import { configureUseAccounts, useAccounts, type AccountState } from '../use-accounts'
+import { configureUseAccounts, useAccounts } from '../use-accounts'
 
-const checkAccountByEmailMock = vi.fn(
-  async () => ({ success: true, exists: true })
-)
+const checkAccountByEmailMock = vi.fn()
 
 configureUseAccounts(checkAccountByEmailMock)
 
 describe('use-accounts', () => {
-  function assertStateData(
-    done: TestContext,
-    key: keyof AccountState,
-    value: string | boolean
-  ): Unsubscriber {
-    function getStateData(state: AccountState): void {
-      done.expect(state).toHaveProperty(key, value)
-    }
-
-    const { unsubscribe } = useAccounts(getStateData)
-
-    return <Unsubscriber>unsubscribe
-  }
-
   let unsubscriber: Unsubscriber | undefined
 
   beforeEach(() => {
@@ -31,30 +15,123 @@ describe('use-accounts', () => {
   })
 
   afterEach(() => {
+    useAccounts().resetState()
+
     unsubscriber && unsubscriber()
   })
 
   describe('setDocument()', () => {
-    test('sets a document to the accounts state', done => {
+    test('sets a document to the accounts state', () => {
       const document = '1234567890'
 
-      const { setDocument } = useAccounts()
+      const { setDocument, getState } = useAccounts()
 
       setDocument(document)
 
-      unsubscriber = assertStateData(done, 'document', document)
+      expect(getState()).toHaveProperty('document', document)
     })
   })
 
   describe('setPassword()', () => {
-    test('sets a password to the accounts state', done => {
+    test('sets a password to the accounts state', () => {
       const password = 'test1234!'
 
-      const { setPassword } = useAccounts()
+      const { setPassword, getState } = useAccounts()
 
       setPassword(password)
 
-      unsubscriber = assertStateData(done, 'password', password)
+      expect(getState()).toHaveProperty('password', password)
+    })
+  })
+
+  describe('checkAccount()', () => {
+    test('checks if there is an account for the given email', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: false })
+
+      const { checkAccount } = useAccounts()
+
+      await checkAccount('test@email.com')
+
+      expect(checkAccountByEmailMock).toHaveBeenCalledTimes(1)
+    })
+
+    test('sets the error message when checking account by email returns success false', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: false })
+
+      const { checkAccount, getState } = useAccounts()
+
+      await checkAccount('error@message.com')
+
+      expect(getState()).toHaveProperty(
+        'errorMessage',
+        'It was not possible to check the account. Please try again.'
+      )
+    })
+
+    test('sets the check account flag to false after check account failed', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: false })
+
+      const { checkAccount, getState } = useAccounts()
+
+      await checkAccount('checkaccount@flag.com')
+
+      expect(getState()).toHaveProperty('isCheckingAccount', false)
+    })
+
+    test('sets the email to the state when the check account is successfull', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: true })
+
+      const { checkAccount, getState } = useAccounts()
+
+      const email = 'accountdonot@exist.com'
+
+      await checkAccount(email)
+
+      expect(getState()).toHaveProperty('email', email)
+    })
+
+    test('sets the account created flag to false when the account does not exist', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: true, exists: false })
+
+      const { checkAccount, getState } = useAccounts()
+
+      const email = 'account@created.com'
+
+      await checkAccount(email)
+
+      expect(getState()).toHaveProperty('isAccountCreated', false)
+    })
+
+    test('sets the account created flag to true when the account already exists', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: true, exists: true })
+
+      const { checkAccount, getState } = useAccounts()
+
+      const email = 'account@created.true'
+
+      await checkAccount(email)
+
+      expect(getState()).toHaveProperty('isAccountCreated', true)
+    })
+
+    test('sets the checking account flag to false after successfully check account', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: true })
+
+      const { checkAccount, getState } = useAccounts()
+
+      await checkAccount('checking@account.com')
+
+      expect(getState()).toHaveProperty('isCheckingAccount', false)
+    })
+
+    test('sets the account already checked flag to true after checking the account with success', async () => {
+      checkAccountByEmailMock.mockResolvedValueOnce({ success: true })
+
+      const { checkAccount, getState } = useAccounts()
+
+      await checkAccount('accountalready@checked.com')
+
+      expect(getState()).toHaveProperty('isAccountAlreadyChecked', true)
     })
   })
 
