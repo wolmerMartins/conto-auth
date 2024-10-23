4d71987d8acc49622320c1bb732042d1702d065f
Wolmer Martins <wolmermartins@hotmail.com>
2024-10-23 13:58:54 -0300

    feat: adds error message to email input

--- src/components/tests/CheckAccount.test.ts
+++ src/components/tests/CheckAccount.test.ts
@@ -1,11 +1,23 @@
 import { render, screen } from '@testing-library/svelte'
 import userEvent from '@testing-library/user-event'
-import { expect, test, describe } from 'vitest'
+import { expect, test, describe, vi } from 'vitest'
 
-import { useAccounts, type AccountState } from '../../hooks/use-accounts'
+import { configureUseAccounts, useAccounts, type AccountState } from '../../hooks/use-accounts'
 import CheckAccount from '../CheckAccount.svelte'
 
 describe('CheckAccount.svelte', () => {
+  const checkAccountByEmailMock = vi.fn(
+    async () => ({ success: true, exists: true })
+  )
+
+  configureUseAccounts(checkAccountByEmailMock)
+
+  const { getState, resetState } = useAccounts()
+
+  afterEach(() => {
+    resetState()
+  })
+
   test('button must be disabled when there is no email typed', async () => {
     render(CheckAccount)
 
@@ -27,6 +39,22 @@ describe('CheckAccount.svelte', () => {
     expect(button).toHaveProperty('disabled', true)
   })
 
+  test('input must display an error message when the email is not valid', async () => {
+    const user = userEvent.setup()
+
+    render(CheckAccount)
+
+    const input = screen.getByLabelText('email')
+    await user.type(input, 'test@email')
+
+    const button = screen.getByRole('button')
+    await user.click(button)
+
+    const error = screen.getByText('The email is not valid')
+
+    expect(error).toBeDefined()
+  })
+
   test('button must not be disabled when there is a email typed', async () => {
     const user = userEvent.setup()
 
@@ -40,7 +68,7 @@ describe('CheckAccount.svelte', () => {
     expect(button).toHaveProperty('disabled', false)
   })
 
-  test('sets the email into the global state when checking the account', async done => {
+  test('sets the email into the global state when checking the account', async () => {
     const email = 'test@email.com'
 
     const user = userEvent.setup()
@@ -53,16 +81,24 @@ describe('CheckAccount.svelte', () => {
     const button = screen.getByRole('button')
     await user.click(button)
 
-    let callNum = 0
-    useAccounts(
-      (state: AccountState): void => {
-        if (callNum < 1) {
-          callNum += 1
-          return
-        }
+    expect(getState()).toHaveProperty('email', email)
+  })
 
-        done.expect(state).toHaveProperty('email', email)
-      }
-    )
+  test('displays an error message when there is an error checking the account', async () => {
+    checkAccountByEmailMock.mockResolvedValueOnce({ success: false, exists: false })
+
+    const user = userEvent.setup()
+
+    render(CheckAccount)
+
+    const input = screen.getByLabelText('email')
+    await user.type(input, 'test@email.com')
+
+    const button = screen.getByRole('button')
+    await user.click(button)
+
+    const error = screen.getByText('It was not possible to check the account. Please try again.')
+
+    expect(error).toBeDefined()
   })
 })
